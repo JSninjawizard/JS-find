e246f59c440786959feff6da3d0747b59095bd5f
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-06-13 23:33:02 +0300

    fff
    To find a way to fix: eye doesn't disappear when clicked in codeInput.eventListener "blur"

--- newUser.js
+++ newUser.js
@@ -1,90 +1,97 @@
-
 //@ Testing Modal New user
-const jobInput = document.querySelector(".job__input")
-const addressInput = document.querySelector(".address__input")
-const cityInput = document.querySelector(".city__input")
-const codeInput = document.querySelector(".code__input")
-const eyes = document.querySelector(".eyes-icon")
-const emailInput = document.querySelector(".email__input")
-const ownerInput = document.querySelector(".owner__input")
-const telInput = document.querySelector(".tel__input")
+const jobInput = document.querySelector(".job__input");
+const addressInput = document.querySelector(".address__input");
+const cityInput = document.querySelector(".city__input");
+const codeInput = document.querySelector(".code__input");
+const eyes = document.querySelector(".eyes-icon");
+const emailInput = document.querySelector(".email__input");
+const ownerInput = document.querySelector(".owner__input");
+const telInput = document.querySelector(".tel__input");
+const regBtn = document.querySelector(".new-user-reg__btn");
 
 jobInput.addEventListener("click", (e) => {
-  jobInput.classList.add('remove')
-
-})
+  jobInput.classList.add("remove-placeholder");
+});
 
 jobInput.addEventListener("blur", (e) => {
-  jobInput.classList.remove('remove')
-
-})
+  jobInput.classList.remove("remove-placeholder");
+});
 
 addressInput.addEventListener("click", (e) => {
-  addressInput.classList.add('remove')
-
-})
+  addressInput.classList.add("remove-placeholder");
+});
 
 addressInput.addEventListener("blur", (e) => {
-  addressInput.classList.remove('remove')
-
-})
+  addressInput.classList.remove("remove-placeholder");
+});
 
 cityInput.addEventListener("click", (e) => {
-  cityInput.classList.add('remove')
-
-})
+  cityInput.classList.add("remove-placeholder");
+});
 
 cityInput.addEventListener("blur", (e) => {
-  cityInput.classList.remove('remove')
-
-})
+  cityInput.classList.remove("remove-placeholder");
+});
 
 codeInput.addEventListener("click", (e) => {
-  codeInput.classList.add('remove')
-    eyes.classList.add('show-eyes')
-})
-
-codeInput.addEventListener("blur", (e) => {
-    codeInput.classList.remove('remove')
-    // eyes.classList.remove('show-eyes')
-})
-
-eyes.addEventListener('click', () => {
-  console.log(codeInput.type);
-  if (codeInput.type === "password") {
-    codeInput.type = 'text'
-  } else if (codeInput.type === "text") {
-    code.type = 'password'
+  codeInput.classList.add("remove-placeholder");
+    // eyes.classList.add("show-eyes");
+  });
+  
+  codeInput.addEventListener('input', (e) => {
+    if (e.target.value !== "") {
+      eyes.classList.add("show-eyes");
+    } else {
+      eyes.classList.remove("show-eyes");
   }
 })
 
-emailInput.addEventListener("click", (e) => {
-  emailInput.classList.add('remove')
+codeInput.addEventListener("blur", (e) => {
+  codeInput.classList.remove("remove-placeholder");
+  // eyes.classList.remove('show-eyes')
+});
 
-})
+eyes.addEventListener("click", () => {
+  // console.log(codeInput.type);
+  if (codeInput.type === "password") {
+    // eyes.classList.add("show-eyes");
+    eyes.classList.remove('fa-eye')
+    eyes.classList.add('fa-eye-slash')
+    codeInput.type = "text";
+  }
+  else if (codeInput.type === 'text') {
+    eyes.classList.remove('fa-eye-slash')
+    eyes.classList.add('fa-eye')
+    codeInput.type = "password";
+  }
+});
+
+emailInput.addEventListener("click", (e) => {
+  emailInput.classList.add("remove-placeholder");
+});
 
 emailInput.addEventListener("blur", (e) => {
-  emailInput.classList.remove('remove')
-
-})
+  emailInput.classList.remove("remove-placeholder");
+});
 
 ownerInput.addEventListener("click", (e) => {
-  ownerInput.classList.add('remove')
-
-})
+  ownerInput.classList.add("remove-placeholder");
+});
 
 ownerInput.addEventListener("blur", (e) => {
-  ownerInput.classList.remove('remove')
-
-})
+  ownerInput.classList.remove("remove-placeholder");
+});
 
 telInput.addEventListener("click", (e) => {
-  telInput.classList.add('remove')
-
-})
+  telInput.classList.add("remove-placeholder");
+});
 
 telInput.addEventListener("blur", (e) => {
-  telInput.classList.remove('remove')
+  telInput.classList.remove("remove-placeholder");
+});
 
+regBtn.addEventListener('click', ()=>{
+  const pizda = []
+  pizda.push(jobInput.value)
+  console.log(pizda);
 })
-
