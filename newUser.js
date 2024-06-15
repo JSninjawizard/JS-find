e58a94b542fd8b639bdcb6ecf84ea164d8e32dbc
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-06-15 21:34:26 +0300

    gg

--- newUser.js
+++ newUser.js
@@ -9,17 +9,17 @@ const ownerInput = document.querySelector(".owner__input");
 const telInput = document.querySelector(".tel__input");
 const regBtn = document.querySelector(".new-user-reg__btn");
 
-const jobDiv = document.querySelector('.job')
-const addressDiv = document.querySelector('.address')
-const cityDiv = document.querySelector('.city')
-const codeDiv = document.querySelector('.code')
-const emailDiv = document.querySelector('.email')
-const ownerDiv = document.querySelector('.owner')
-const telDiv = document.querySelector('.tel')
+const jobDiv = document.querySelector(".job");
+const addressDiv = document.querySelector(".address");
+const cityDiv = document.querySelector(".city");
+const codeDiv = document.querySelector(".code");
+const emailDiv = document.querySelector(".email");
+const ownerDiv = document.querySelector(".owner");
+const telDiv = document.querySelector(".tel");
 
 jobInput.addEventListener("click", (e) => {
   jobInput.classList.add("remove-placeholder");
-  jobDiv.classList.remove('not-validated')
+  jobDiv.classList.remove("not-validated");
 });
 
 jobInput.addEventListener("blur", (e) => {
@@ -50,13 +50,13 @@ codeInput.addEventListener("click", (e) => {
   // eyes.classList.add("show-eyes");
 });
 
-codeInput.addEventListener('input', (e) => {
+codeInput.addEventListener("input", (e) => {
   if (e.target.value !== "") {
     eyes.classList.add("show-eyes");
   } else {
     eyes.classList.remove("show-eyes");
   }
-})
+});
 
 codeInput.addEventListener("blur", (e) => {
   codeInput.classList.remove("remove-placeholder");
@@ -68,13 +68,12 @@ eyes.addEventListener("click", () => {
   // console.log(codeInput.type);
   if (codeInput.type === "password") {
     // eyes.classList.add("show-eyes");
-    eyes.classList.remove('fa-eye')
-    eyes.classList.add('fa-eye-slash')
+    eyes.classList.remove("fa-eye");
+    eyes.classList.add("fa-eye-slash");
     codeInput.type = "text";
-  }
-  else if (codeInput.type === 'text') {
-    eyes.classList.remove('fa-eye-slash')
-    eyes.classList.add('fa-eye')
+  } else if (codeInput.type === "text") {
+    eyes.classList.remove("fa-eye-slash");
+    eyes.classList.add("fa-eye");
     codeInput.type = "password";
   }
 });
@@ -106,94 +105,106 @@ telInput.addEventListener("blur", (e) => {
   telInput.classList.remove("remove-placeholder");
 });
 
-regBtn.addEventListener('click', ()=>{
+regBtn.addEventListener("click", () => {
   if (
-    jobInput.value === "" || 
-    addressInput.value === "" || 
-    cityInput.value === "" || 
-    codeInput.value === "" || 
-    emailInput.value === "" || 
-    ownerInput.value === "" || 
+    jobInput.value === "" ||
+    addressInput.value === "" ||
+    cityInput.value === "" ||
+    Number(codeInput.value) === "" ||
+    emailInput.value === "" ||
+    ownerInput.value === "" ||
     telInput.value === ""
   ) {
-    validateNewUser()
+    validateNewUser();
+  } 
+  else if (
+    jobInput.value !== "" &&
+    addressInput.value !== "" &&
+    cityInput.value !== "" &&
+    Number(codeInput.value) !== "" &&
+    emailInput.value !== "" &&
+    ownerInput.value !== "" &&
+    telInput.value !== ""
+  ) {
+    validatedNewUser ()
   }
-  else {
-  const account = new Object()
-  account.job = jobInput.value
-  account.address = addressInput.value
-  account.city = cityInput.value
-  account.code = codeInput.value
-  account.email = emailInput.value
-  account.owner = ownerInput.value
-  account.tel = telInput.value
-
-  accounts.push(account)
-  createAcronym(accounts);
-  console.log(accounts);
-  
+  else 
+  {
+    const account = new Object();
+    account.job = jobInput.value;
+    account.address = addressInput.value;
+    account.city = cityInput.value;
+    account.code = Number(codeInput.value);
+    account.email = emailInput.value;
+    account.owner = ownerInput.value;
+    account.tel = telInput.value;
+    
+    accounts.push(account);
+    createAcronym(accounts);
+    console.log(accounts);
   }
-})
-
+});
 
 function validateNewUser() {
-    if (jobInput.value !== "") {
-      jobDiv.classList.remove('not-validated')
-      jobDiv.classList.add('validated')
-    } 
-    else {
-      jobDiv.classList.remove('validated')
-      jobDiv.classList.add('not-validated')
-    }
-    
-    if (addressInput.value !== "") {
-      addressDiv.classList.remove('not-validated')
-      addressDiv.classList.add('validated')
-    } 
-    else {
-      addressDiv.classList.remove('validated')
-      addressDiv.classList.add('not-validated')
-    }
-    
-    if (cityInput.value !== "") {
-      cityDiv.classList.add('validated')
-    } 
-    else {
-      cityDiv.classList.remove('validated')
-      cityDiv.classList.add('not-validated')
-    }
-
-    if (codeInput.value !== "") {
-      codeDiv.classList.add('validated')
-    } 
-    else {
-      codeDiv.classList.remove('validated')
-      codeDiv.classList.add('not-validated')
-    }
-
-    if (emailInput.value !== "") {
-      emailDiv.classList.add('validated')
-    } 
-    else {
-      emailDiv.classList.remove('validated')
-      emailDiv.classList.add('not-validated')
-    }
-
-    if (ownerInput.value !== "") {
-      ownerDiv.classList.add('validated')
-    } 
-    else {
-      ownerDiv.classList.remove('validated')
-      ownerDiv.classList.add('not-validated')
-    }
-
-    if (telInput.value !== "") {
-      telDiv.classList.add('validated')
-    } 
-    else {
-      telDiv.classList.remove('validated')
-      telDiv.classList.add('not-validated')
-    }
+  if (jobInput.value !== "") {
+    jobDiv.classList.remove("not-validated");
+    jobDiv.classList.add("validated");
+  } else {
+    jobDiv.classList.remove("validated");
+    jobDiv.classList.add("not-validated");
+  }
+  
+  if (addressInput.value !== "") {
+    addressDiv.classList.remove("not-validated");
+    addressDiv.classList.add("validated");
+  } else {
+    addressDiv.classList.remove("validated");
+    addressDiv.classList.add("not-validated");
+  }
+  
+  if (cityInput.value !== "") {
+    cityDiv.classList.add("validated");
+  } else {
+    cityDiv.classList.remove("validated");
+    cityDiv.classList.add("not-validated");
+  }
+  
+  if (codeInput.value !== "") {
+    codeDiv.classList.add("validated");
+  } else {
+    codeDiv.classList.remove("validated");
+    codeDiv.classList.add("not-validated");
+  }
+  
+  if (emailInput.value !== "") {
+    emailDiv.classList.add("validated");
+  } else {
+    emailDiv.classList.remove("validated");
+    emailDiv.classList.add("not-validated");
+  }
+  
+  if (ownerInput.value !== "") {
+    ownerDiv.classList.add("validated");
+  } else {
+    ownerDiv.classList.remove("validated");
+    ownerDiv.classList.add("not-validated");
+  }
+  
+  if (telInput.value !== "") {
+    telDiv.classList.add("validated");
+  } else {
+    telDiv.classList.remove("validated");
+    telDiv.classList.add("not-validated");
+  }
+}
 
 
+function validatedNewUser () {
+  jobDiv.classList.add("validated");
+  addressDiv.classList.add("validated");
+  cityDiv.classList.add("validated");
+  codeDiv.classList.add("validated");
+  emailDiv.classList.add("validated");
+  ownerDiv.classList.add("validated");
+  telDiv.classList.add("validated");
 }
