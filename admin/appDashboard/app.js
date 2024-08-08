100a969d581b059b94e6d4139e8cfc47bd9ceec2
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-08-09 00:33:15 +0300

    new

--- admin/appDashboard/app.js
+++ admin/appDashboard/app.js
@@ -1,1 +1,8 @@
 const hamburger = document.querySelector('.vertical-nav--burger')
+const container = document.querySelector('.container')
+
+
+
+hamburger.addEventListener('click', () => {
+    container.classList.toggle('exp')
+})
